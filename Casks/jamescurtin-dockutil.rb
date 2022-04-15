8b0b7e01c14c27d8f2c2671bc0e616af57eb4290
James Curtin <jacurtin@wayfair.com>
2022-04-15 18:56:42 -0400

    Add dockutil cask

---
+++ Casks/jamescurtin-dockutil.rb
@@ -0,0 +1,13 @@
+cask "jamescurtin-dockutil" do
+  version "3.0.2"
+  sha256 "175137ea747e83ed221d60b18b712b256ed31531534cde84f679487d337668fd"
+
+  url "https://github.com/kcrawford/dockutil/releases/download/#{version}/dockutil-#{version}.pkg"
+  name "dockutil"
+  desc "Tool for managing dock items"
+  homepage "https://github.com/kcrawford/dockutil"
+
+  pkg "dockutil-#{version}.pkg"
+
+  uninstall pkgutil: "dockutil.cli.tool"
+end
